45b1c0841b22236f7c157e83e66bc8eb1074d42e
Mileide-Mikka <mi-leyde@hotmail.com>
2022-07-29 21:43:49 -0300

    simulador finalizado, com requezição de nome e velocidade para nave.

--- script.js
+++ script.js
@@ -4,10 +4,26 @@ let velocity = 0;
 
 let newVelocity = prompt("A qual velocidade voce gostaria de acelerar?");
 
-let confirmVelocity = confirm("Estamos acelerando para " + newVelocity + "Km/s")
+let confirmVelocity = confirm(
+  "Estamos acelerando para " + newVelocity + "Km/s"
+); //confirma o valor da velocidade
 
-if(confirmVelocity){
-    velocity = newVelocity;
+if (confirmVelocity) {
+  velocity = newVelocity;
+}
+// verificando o nivel da velocidade
+if (velocity <= 0) {
+  alert("Nave se encontra em repouso, considere aumentar a velocidade...");
+} else if (velocity < 40) {
+  alert("Velocidade baixa. Podemos aumentar mais.");
+} else if (velocity < 80) {
+  alert("Velocidade adequada, recomenda-se mante-la!");
+} else if (velocity < 100) {
+  alert("Velocidade alta. Considere reduzi-la...");
+} else {
+  alert(
+    "Você chegou a uma velocidade de risco, devido a isso o piloto automatico foi ativado!"
+  );
 }
 
-console.log(velocity);
+alert("Piloto: " + piloName + "\nVelocidade: " + velocity + "Km/s");
